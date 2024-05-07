d133fde13ad505b15794e6d9fb4dfc949ef5ec57
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-05-07 09:56:09 +0530

    Created model Contact

--- crm_project/crm/models.py
+++ crm_project/crm/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    fist_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.fist_name + " " + self.last_name
