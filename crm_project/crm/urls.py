a05925a8450ce45e0c471862b5c810eb2c5ffc2b
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-05-08 02:02:48 +0530

    Updated URL configuration

--- crm_project/crm/urls.py
+++ crm_project/crm/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("record/<int:pk>/", views.view_record, name="view-record"),
+    path("record/create/", views.create_record, name="create-record"),
+    path("record/<int:pk>/update", views.update_record, name="update-record"),
+    path("record/<int:pk>/delete", views.delete_record, name="delete-record"),
 ]
