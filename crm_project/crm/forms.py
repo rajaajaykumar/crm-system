034e2db9a13a8460f7f6edd43e533dab5fd34947
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-05-07 10:11:28 +0530

    Created forms.py

---
+++ crm_project/crm/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+from django import forms
+from .models import Contact
+
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "password1", "password2"]
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
+class CreateUpdateContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ["first_name", "last_name", "email", "phone", "address", "city", "state", "country"]
