4f46c1aa347b35d0767c0836f04cdace297343d8
rajaajaykumar <rajaajaykumar6@gmail.com>
2024-05-08 02:00:31 +0530

    Updated model Contact

--- crm_project/crm/models.py
+++ crm_project/crm/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class Contact(models.Model):
-    fist_name = models.CharField(max_length=100)
+    class Role(models.TextChoices):
+        ADMIN = 'Admin', 'Admin'
+        USER = 'User', 'User'
+        GUEST = 'Guest', 'Guest'
+
+    first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
@@ -12,4 +18,4 @@ class Contact(models.Model):
     creation_data = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.fist_name + " " + self.last_name
+        return f"{self.first_name} {self.last_name} | {self.role}"
